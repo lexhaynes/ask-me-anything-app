2007372dcc967133b15e4374643dffc7a744f43c
Alexandra Haynes <alex@Alex.local>
2016-04-09 15:25:31 -0400

    added app bar and lef nav

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -1,17 +1,57 @@
 import React from "react"
-import classnames from 'classnames'
+//MUI
+import ThemeManager from 'material-ui/lib/styles/getMuiTheme';
+import Theme from './Theme.js'
+//MUI Components
+import AppBar from 'material-ui/lib/app-bar'
+import LeftNav from 'material-ui/lib/left-nav'
 
 
-export default (props) => {
-    return (
-      
-  <div className="page-content">
+const PageTemplate = React.createClass({  
 
-  {/* PAGE CONTENT  */}
-    {props.content}
-  {/* END PAGE CONTENT  */}
+  childContextTypes : {
+        muiTheme: React.PropTypes.object
+    },
 
-  </div>
-    
-    );
-}
+  getInitialState() {
+    return {
+      leftNavOpen: false
+    }
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager(Theme),
+    }
+  },
+
+  toggleNav() {
+    this.setState({
+      leftNavOpen: !this.state.leftNavOpen
+    })
+  },
+
+
+  render() {
+    return (        
+      <div className="page-content">
+
+      {/* PAGE CONTENT  */}
+      <AppBar 
+        onLeftIconButtonTouchTap = {this.toggleNav}
+      />
+      <LeftNav 
+        open = {this.state.leftNavOpen}
+        docked = {false}
+        onRequestChange = {this.toggleNav}
+      />
+        {this.props.content}
+      {/* END PAGE CONTENT  */}
+
+      </div>
+    )
+  }
+})
+
+export default PageTemplate;
+

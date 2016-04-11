cfe60c25b9584a8342ff1a156294275fd0d5492b
Alex Haynes <lex.haynes@gmail.com>
2016-04-11 16:25:39 -0400

    commented out css loaders in webpack prod config file to see if that will help app deploy

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,13 +13,13 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true })
+   // new ExtractTextPlugin('style.css', { allChunks: true })
   ],
 
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
+     // { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
     ]
   },
 

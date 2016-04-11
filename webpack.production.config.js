7b5ea72c983fb725bb2a1796f81c5db92e238c99
Alex Haynes <lex.haynes@gmail.com>
2016-04-11 16:55:57 -0400

    spaces

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,7 +13,8 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }    ]
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }    
+      ]
   },
 
   resolve: {

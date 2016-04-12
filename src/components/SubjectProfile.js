1e32fa00814b38670c48658a6a03dc1302c2ab6e
Alex Haynes <lex.haynes@gmail.com>
2016-04-12 15:39:01 -0400

    added dummy data to initial state

--- src/components/SubjectProfile.js
+++ src/components/SubjectProfile.js
@@ -18,8 +18,8 @@ export default class SubjectProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Hi! I'm Aziz Ansari. Ask me anything...",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      title: "We are Aziz Ansari and Alan Yang from Master of None - Ask Us Anything",
+      description: "It's Aziz Ansari. I'm here to chat about my new Netflix series Master of None. I have my co-creator/writer Alan Yang (/u/ItsAlanYang) with me here too. We are so humbled by how much you guys have supported the show's first season. The online community has been so nice to the show. So thank you!",
       mode: this.props.mode
     }
     this.updateTitle = this.updateTitle.bind(this)

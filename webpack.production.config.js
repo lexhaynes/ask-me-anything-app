411fb60cf52d4b8a2f27d03edc835ae261ad72c3
Alex Haynes <lex.haynes@gmail.com>
2016-04-12 14:44:35 -0400

    added production flag to webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,6 +17,12 @@ module.exports = {
     ]
   },
 
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+  })
+  ],
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   }

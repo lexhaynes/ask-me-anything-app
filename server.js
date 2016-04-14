7084121061001a57cf1c38f296d35663fab16000
Alex Haynes <lex.haynes@gmail.com>
2016-04-14 11:41:19 -0400

    updated server for dev

--- server.js
+++ server.js
@@ -14,19 +14,28 @@ var app = express();
 // Serve application file depending on environment
 console.log('node env', process.env.NODE_ENV)
 app.get('/app.js', (req, res) => {
+  if (process.env.PRODUCTION || process.env.NODE_ENV === "production") {
     res.sendFile(__dirname + '/build/app.js');
-
+  } else {
+    res.redirect('//localhost:9090/build/app.js');
+  }
 });
 
 // Serve aggregate stylesheet depending on environment
 app.get('/css/styles.css', (req, res) => {
+  if (process.env.PRODUCTION || process.env.NODE_ENV === "production") {
     res.sendFile(__dirname + '/build/css/styles.css');
-  
+  } else {
+    res.redirect('//localhost:9090/build/css/styles.css');
+  }
 });
 
 app.get('/favicon.png', (req, res) => {
+  if (process.env.PRODUCTION || process.env.NODE_ENV === "production") {
     res.sendFile(__dirname + '/build/favicon.png');
-  
+  } else {
+    res.redirect('//localhost:9090/build/favicon.png');
+  }
 });
 
 // Serve index page
@@ -35,7 +44,30 @@ app.get('*', (req, res) => {
 });
 
 
+/*************************************************************
+ *
+ * Webpack Dev Server
+ *
+ * See: http://webpack.github.io/docs/webpack-dev-server.html
+ *
+ *************************************************************/
 
+if (!process.env.PRODUCTION || process.env.NODE_ENV !== "production") {
+  const webpack = require('webpack');
+  const WebpackDevServer = require('webpack-dev-server');
+  const config = require('./webpack.local.config');
+
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    noInfo: true,
+    historyApiFallback: true
+  }).listen(9090, 'localhost', (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
 
 
 /******************

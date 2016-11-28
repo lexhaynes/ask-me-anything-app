9dd2299b8fa7df93dd28d4bac544409721294359
Alex Haynes <alex@Alex.local>
2016-11-28 12:34:20 -0500

    requiring Home

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import App from './App';
-import Home from '../pages/home'
-import Admin from '../pages/admin'
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+import App from './App'
+var Home = require('../pages/home');
+var Admin = require('../pages/admin');
+/*import Home from '../pages/home'
+import Admin from '../pages/admin'*/
 
 export default (
   <Route path="/" component={App}>

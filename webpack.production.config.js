fcc1f837076ab5c20472980e51a37f2849d2d297
Alex Haynes <alex@Alex.local>
2016-11-28 12:57:58 -0500

    revert

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: "babel-loader" }
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
 

85dec5d7aae6726e8e75af26f0d26ddad0b9c3dc
Alex Haynes <alex@Alex.local>
2016-11-28 12:45:42 -0500

    build

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './App'
-import Home from '/src/components/pages/home'
+import Home from '../pages/home'
 import Admin from '../pages/admin'
 
 export default (
